8972c7103f87e8c9781a8a03e7c1010f0a761e87
Natalia Bannikova <natabannikova10@gmail.com>
2020-08-24 07:03:01 +0300

    Add slider content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 
-$('.slider__inner').slick();
+$('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+});
 
 
 
