e68b59cacc2808179b236c4ceb516127aa92d52e
Natalia Bannikova <natabannikova10@gmail.com>
2020-09-09 18:13:19 +0300

    Add HTML and JS for news__slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-$('.slider__inner').slick({
+$('.slider__inner, .news__slider-inner').slick({
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     infinite: false
